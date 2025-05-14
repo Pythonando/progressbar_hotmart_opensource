75291d4c5a92bf4451f73be90b62bcad4279b295
Pythonando <contato.pycursos@gmail.com>
2025-05-14 19:38:50 -0300

    commit

--- progressbar/urls.py
+++ progressbar/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/h/<str:offer_code>/', views.progressbar, name='progressbar'),
+    path('h/<str:offer_code>/', views.progressbar, name='progressbar'),
     path('webhook_hotmart/', views.webhook_hotmart, name="webhook_hotmart"),
 ]
